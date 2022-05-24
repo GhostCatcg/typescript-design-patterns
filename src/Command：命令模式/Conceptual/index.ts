4e34683925bda0a9d64d5c81382ea797b98ac0fa
GhostCat <gcatcg@gmail.com>
2022-05-24 22:33:17 +0800

    feat: 学习状态模式

--- src/Command：命令模式/Conceptual/index.ts
+++ src/Command：命令模式/Conceptual/index.ts
@@ -1,14 +1,18 @@
 /**
  * Command Design Pattern
+ * 命令模式 - 设计模式
  *
  * Intent: Turns a request into a stand-alone object that contains all
  * information about the request. This transformation lets you parameterize
  * methods with different requests, delay or queue a request's execution, and
  * support undoable operations.
+ * Intent:将一个请求转换为一个独立的对象，其中包含该请求的所有信息。
+ * 此转换允许您对具有不同请求的方法进行参数化，延迟或对请求的执行进行排队，并支持可撤销的操作。
  */
 
 /**
  * The Command interface declares a method for executing a command.
+ * Command接口声明用于执行命令的方法。
  */
 interface Command {
     execute(): void;
@@ -16,6 +20,7 @@ interface Command {
 
 /**
  * Some commands can implement simple operations on their own.
+ * 有些命令可以单独实现简单的操作。
  */
 class SimpleCommand implements Command {
     private payload: string;
@@ -32,12 +37,14 @@ class SimpleCommand implements Command {
 /**
  * However, some commands can delegate more complex operations to other objects,
  * called "receivers."
+ * 然而，一些命令可以将更复杂的操作委托给其他对象，称为“接收者”。
  */
 class ComplexCommand implements Command {
     private receiver: Receiver;
 
     /**
      * Context data, required for launching the receiver's methods.
+     * 上下文数据，用于启动接收方的方法。
      */
     private a: string;
 
@@ -46,6 +53,7 @@ class ComplexCommand implements Command {
     /**
      * Complex commands can accept one or several receiver objects along with
      * any context data via the constructor.
+     * 复杂命令可以通过构造函数接受一个或多个接收器对象以及任何上下文数据。
      */
     constructor(receiver: Receiver, a: string, b: string) {
         this.receiver = receiver;
@@ -55,6 +63,7 @@ class ComplexCommand implements Command {
 
     /**
      * Commands can delegate to any methods of a receiver.
+     * 命令可以委托给接收者的任何方法。
      */
     public execute(): void {
         console.log('ComplexCommand: Complex stuff should be done by a receiver object.');
@@ -67,6 +76,8 @@ class ComplexCommand implements Command {
  * The Receiver classes contain some important business logic. They know how to
  * perform all kinds of operations, associated with carrying out a request. In
  * fact, any class may serve as a Receiver.
+ * Receiver类包含一些重要的业务逻辑。他们知道如何执行与执行请求相关的各种操作。
+ * 事实上，任何类都可以用作Receiver。
  */
 class Receiver {
     public doSomething(a: string): void {
@@ -81,6 +92,8 @@ class Receiver {
 /**
  * The Invoker is associated with one or several commands. It sends a request to
  * the command.
+ * Invoker与一个或多个命令相关联。
+ * 它向命令发送一个请求。
  */
 class Invoker {
     private onStart: Command;
@@ -89,6 +102,7 @@ class Invoker {
 
     /**
      * Initialize commands.
+     * 初始化命令。
      */
     public setOnStart(command: Command): void {
         this.onStart = command;
@@ -102,6 +116,8 @@ class Invoker {
      * The Invoker does not depend on concrete command or receiver classes. The
      * Invoker passes a request to a receiver indirectly, by executing a
      * command.
+     * Invoker不依赖于具体的命令或接收器类。
+     * 调用程序通过执行命令间接地将请求传递给接收者。
      */
     public doSomethingImportant(): void {
         console.log('Invoker: Does anybody want something done before I begin?');
@@ -124,6 +140,7 @@ class Invoker {
 
 /**
  * The client code can parameterize an invoker with any commands.
+ * 客户端代码可以用任何命令对调用程序进行参数化。
  */
 const invoker = new Invoker();
 invoker.setOnStart(new SimpleCommand('Say Hi!'));
